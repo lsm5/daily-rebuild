9aca91573d74558a5c3f8876aa3835a00ba99746
Lokesh Mandvekar <lsm5@fedoraproject.org>
2015-03-01 14:39:49 -0600

    update golang import paths provided via go2fed
    Signed-off-by: Lokesh Mandvekar <lsm5@fedoraproject.org>

--- docker-daily.sh
+++ docker-daily.sh
@@ -1,19 +1,5 @@
 #!/bin/sh
 
-# enter docker dir
-cd ~/repositories/github.com/docker/docker
-
-# checkout master branch
-git checkout master
-
-# update master branch from remote
-git pull
-
-# export commit and version values
-export GITCOMMIT=$(git show --pretty=%H)
-export SHORTCOMMIT=$(c=$GITCOMMIT; echo ${c:0:7})
-export VERSION=$(sed -e 's/-.*//' VERSION)
-
 # enter docker rpm dir
 cd ~/repositories/pkgs/fedora/docker-io
 
@@ -23,6 +9,23 @@ rm -rf *.tar.gz RPMS SRPMS BUILD*
 # checkout rpm master branch and pull from remote
 git checkout master
 git pull
+
+# enter docker dir
+cd ~/repositories/github.com/docker/docker
+
+# checkout master branch
+git checkout master
+
+# update master branch from remote
+git pull
+
+
+# export commit and version values
+export GITCOMMIT=$(git show --pretty=%H)
+export SHORTCOMMIT=$(c=$GITCOMMIT; echo ${c:0:7})
+export VERSION=$(sed -e 's/-.*//' VERSION)
+
+cd ~/repositories/pkgs/fedora/docker-io
 
 # if existing version in spec file is not equal to latest version, set
 # release tag value 0
@@ -49,15 +52,36 @@ fi
 export RELEASE=$(cat docker-io.spec | grep "Release:" | \
     sed -e "s/Release:\t//")
 
-#-------
-# Fedora
-#-------
-
 # build rpm without running check
 rpmbuild -ba docker-io.spec --nocheck
 
+pushd BUILD/docker-$GITCOMMIT
+rm -rf vendor
+
+# print all golang paths
+# for each golang path, if it exists in spec file, continue
+# else, add the golang path just below the Summary: line
+# (skip vendor/ paths)
+for line in $(go2fed inspect -p)
+do
+    if grep -Fxq "Provides: golang(%{import_path}/$line) = %{version}-%{release}" \
+        ~/repositories/pkgs/fedora/docker-io/docker-io.spec
+    then
+        continue
+    else
+        sed -i "/Summary:  A golang registry/a Provides: golang(%{import_path}/$line) = %{version}-%{release}" \
+            ~/repositories/pkgs/fedora/docker-io/docker-io.spec
+    fi
+done
+
+popd
+
 # reset everything before import
 git reset --hard
+
+#-------
+# Fedora
+#-------
 
 # fedpkg import SRPM
 fedpkg import --skip-diffs SRPMS/docker-io-$VERSION-*.src.rpm
@@ -79,9 +103,9 @@ git push -u origin master
 # build package in koji
 fedpkg build
 
-#--------
+#-------
 # CentOS
-#--------
+#-------
 
 # remove stale docker-master.spec
 rm docker-master.spec
@@ -99,4 +123,4 @@ sed -i 's/selinux\"/selinux btrfs_noversion\"/' docker-master.spec
 rpmbuild --define 'dist .el7' -ba docker-master.spec --nocheck
 
 # send to CBS (ckbuild = koji build virt7-el7)
-ckbuild SRPMS/docker-master-*.el7.src.rpm
+~/bin/ckoji build virt7-el7 SRPMS/docker-master-*.el7.src.rpm
