58af33b475fd6f9e992eb5e82f0758faa7337dea
Lokesh Mandvekar <lsm5@fedoraproject.org>
2015-03-29 01:00:09 -0500

    replace docker-io with docker
    Signed-off-by: Lokesh Mandvekar <lsm5@fedoraproject.org>

--- docker-daily.sh
+++ docker-daily.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # enter docker rpm dir
-cd ~/repositories/pkgs/fedora/docker-io
+cd ~/repositories/pkgs/fedora/docker
 
 # delete stale packages, tarballs and build dirs
 rm -rf *.tar.gz RPMS SRPMS BUILD*
@@ -23,7 +23,7 @@ git fetch rhatdan
 git checkout rhatdan/fedora
 
 # rebase @rhatdan/fedora on origin/master
-git rebase master
+git rebase origin/master
 
 # if rebase fails, email patch author with output of 'git diff'
 # abort the rebase and exit
@@ -54,34 +54,35 @@ export GITCOMMIT=$(git show --pretty=%H -s)
 export SHORTCOMMIT=$(c=$GITCOMMIT; echo ${c:0:7})
 export VERSION=$(sed -e 's/-.*//' VERSION)
 
-cd ~/repositories/pkgs/fedora/docker-io
+cd ~/repositories/pkgs/fedora/docker
 
 # get current version in rpm
-export CURRENT_VERSION=$(cat docker-io.spec | grep "Version:" | \
-    sed -e "s/Version:\t//")
+export CURRENT_VERSION=$(cat docker.spec | grep "Version:" | \
+    sed -e "s/Version: //")
 
 # update spec files with latest values
-sed -i "s/\%global commit.*/\%global commit\t\t$GITCOMMIT/" docker-io.spec
+sed -i "s/\%global d_commit.*/\%global d_commit $GITCOMMIT/" docker.spec
 
 # fetch docker master branch tarball
-spectool -g docker-io.spec
+spectool -g docker.spec
 
 # install deps
-sudo yum-builddep -y docker-io.spec
+sudo yum-builddep -y docker.spec
 
 # update spec changelog and release value
 if [ "$CURRENT_VERSION" == "$VERSION" ]; then
-    rpmdev-bumpspec -c "built commit#$SHORTCOMMIT" docker-io.spec
+    rpmdev-bumpspec -c "built docker @lsm5/fedora commit#$SHORTCOMMIT"  docker.spec
 else
-    rpmdev-bumpspec -n $VERSION -c "New version: $VERSION, built commit#$SHORTCOMMIT" docker-io.spec
-    sed -i "s/Release:    1\%{?dist}/Release:\t1.git\%{shortcommit}\%{?dist}/" docker-io.spec
+    rpmdev-bumpspec -n $VERSION -c "New version: $VERSION, built docker \
+        @lsm5/commit#$SHORTCOMMIT" docker.spec
+    sed -i "s/Release: 1\%{?dist}/Release: 1.git\%{d_shortcommit}\%{?dist}/" docker.spec
 fi
 
-export RELEASE=$(cat docker-io.spec | grep "Release:" | \
-    sed -e "s/Release:\t//")
+export RELEASE=$(cat docker.spec | grep "Release:" | \
+    sed -e "s/Release: //")
 
 # build rpm without running check
-rpmbuild -ba docker-io.spec --nocheck
+rpmbuild -ba docker.spec --nocheck
 
 pushd BUILD/docker-$GITCOMMIT
 rm -rf vendor
@@ -93,12 +94,12 @@ rm -rf vendor
 for line in $(go2fed inspect -p)
 do
     if grep -Fxq "Provides: golang(%{import_path}/$line) = %{version}-%{release}" \
-        ~/repositories/pkgs/fedora/docker-io/docker-io.spec
+        ~/repositories/pkgs/fedora/docker/docker.spec
     then
         continue
     else
         sed -i "/Summary:  A golang registry/a Provides: golang(%{import_path}/$line) = %{version}-%{release}" \
-            ~/repositories/pkgs/fedora/docker-io/docker-io.spec
+            ~/repositories/pkgs/fedora/docker/docker.spec
     fi
 done
 
@@ -112,7 +113,7 @@ git reset --hard
 #-------
 
 # fedpkg import SRPM
-fedpkg import --skip-diffs SRPMS/docker-io-$VERSION-*.src.rpm
+fedpkg import --skip-diffs SRPMS/docker-$VERSION-*.src.rpm
 
 # get NVR
 pushd SRPMS
@@ -138,14 +139,14 @@ fedpkg build
 # remove stale docker-master.spec
 rm docker-master.spec
 
-# copy latest docker-io.spec to docker-master.spec
-cp docker-io.spec docker-master.spec
+# copy latest docker.spec to docker-master.spec
+cp docker.spec docker-master.spec
 
 # change package name for docker-master
-sed -i "s/\%{repo}-io/docker-master/" docker-master.spec
+sed -i "s/Name: \%{repo}/Name: \%{repo}-master/" docker-master.spec
 
 # modify build tags for el7
-#sed -i 's/selinux\"/selinux btrfs_noversion\"/' docker-master.spec
+sed -i 's/selinux\"/selinux btrfs_noversion\"/' docker-master.spec
 
 # build for el7
 rpmbuild --define 'dist .el7' -ba docker-master.spec --nocheck
