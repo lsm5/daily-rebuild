0bcf2a0d1a900b3d1ff1d6298ecaf52a39b9b0e7
Lokesh Mandvekar <lsm5@fedoraproject.org>
2016-01-11 14:26:29 +0000

    update rkt builder
    Signed-off-by: Lokesh Mandvekar <lsm5@fedoraproject.org>

--- pkgs/rkt.sh
+++ pkgs/rkt.sh
@@ -1,133 +1,51 @@
 #!/bin/sh
 
-# enter rkt rpm dir
-cd ~/repositories/pkgs/wip/rkt
+. env.sh
 
-# delete stale packages, tarballs and build dirs
-rm -rf *.tar.gz RPMS SRPMS BUILD*
+# update sources
+update_sources_and_spec ()
+{
+    pushd $REPO_DIR/$PACKAGE
+    git fetch --all
+    git checkout $BRANCH
+    export COMMIT=$(git show --pretty=%H -s $BRANCH)
+    export SHORTCOMMIT=$(c=$COMMIT; echo ${c:0:7})
+    export VERSION=$(grep AC_INIT configure.ac | cut -d' ' -f2 \
+        | tr -d \]\[, | tr -d +git)
+    popd
 
-# checkout rpm master branch and pull from remote
-git checkout master
-git pull
+    pushd $PKG_DIR/$PACKAGE
+    git checkout $DIST_GIT_TAG
+    sed -i "s/\%global commit0.*/\%global commit0 $COMMIT/" $PACKAGE.spec
 
-# enter rkt dir
-cd ~/repositories/github.com/coreos/rkt
+    echo "- built commit#$SHORTCOMMIT" > /tmp/$PACKAGE.changelog
+    popd
+}
 
-# fetch the latest from rkt upstream
-git checkout master
-git pull
-
-# export commit and version values
-export GITCOMMIT=$(git show --pretty=%H -s)
-export SHORTCOMMIT=$(c=$GITCOMMIT; echo ${c:0:7})
-export VERSION=$(grep 'const Version' version/version.go \
-    | sed -e 's/const Version = "//' -e 's/+git"//')
-
-cd ~/repositories/pkgs/wip/rkt
-
-# get current version in rpm
-export CURRENT_VERSION=$(cat rkt.spec | grep "Version:" | \
-    sed -e "s/Version: //")
-
-# update spec files with latest values
-sed -i "s/\%global commit.*/\%global commit $GITCOMMIT/" rkt.spec
-
-# fetch rkt master branch tarball
-spectool -g rkt.spec
-
-# install deps
-sudo yum-builddep -y rkt.spec
-
-# update spec changelog and release value
-if [ "$CURRENT_VERSION" == "$VERSION" ]; then
-    rpmdev-bumpspec -c "built rkt commit#$SHORTCOMMIT"  rkt.spec
-else
-    rpmdev-bumpspec -n $VERSION -c "New version: $VERSION, built rkt \
-        commit#$SHORTCOMMIT" rkt.spec
-    sed -i "s/Release: 1\%{?dist}/Release: 1.git\%{shortcommit}\%{?dist}/" rkt.spec
-fi
-
-export RELEASE=$(cat rkt.spec | grep "Release:" | \
-    sed -e "s/Release: //")
-
-# build rpm without running check
-rpmbuild -ba rkt.spec --nocheck
-
-#pushd BUILD/rkt-$GITCOMMIT
-#rm -rf vendor
-
-# print all golang paths
-# for each golang path, if it exists in spec file, continue
-# else, add the golang path just below the Summary: line
-# (skip vendor/ paths)
-#for line in $(go2fed inspect -p)
-#do
-#    if grep -Fxq "Provides: golang(%{import_path}/$line) = %{version}-%{release}" \
-#        ~/repositories/pkgs/wip/rkt/rkt.spec
-#    then
-#        continue
-#    else
-#        sed -i "/Summary:  A golang registry/a Provides: golang(%{import_path}/$line) = %{version}-%{release}" \
-#            ~/repositories/pkgs/wip/rkt/rkt.spec
-#    fi
-#done
-
-#popd
-
-# reset everything before import
-#git reset --hard
-
-#-------
-# Fedora
-#-------
-
-# fedpkg import SRPM
-#fedpkg import --skip-diffs SRPMS/rkt-$VERSION-*.src.rpm
-
-# get NVR
-pushd SRPMS
-export NVR=$(ls | sed -e "s/\.fc.*//")
-popd
-
-# commit changes after importing
-git commit -asm "NVR: $NVR"
-
-# push changes to github master
-#git push -u github master
-
-# push changes to fedora-cloud/rkt-rpm master
-git push -u origin master
-
-# send SRPM to fedorapeople publicly accessible URL
-scp SRPMS/$NVR.fc23.src.rpm fpeople:public_html/rkt/.
-
-# copr build
-copr-cli build rkt https://lsm5.fedorapeople.org/rkt/$NVR.fc23.src.rpm
-
-# sleep 10 seconds to let koji recognize the latest NVR
-#sleep 10
-
-# build package in koji
-#fedpkg build
 
 #-------
 # CentOS
 #-------
 
-# remove stale rkt-master.spec
-#rm rkt-master.spec
+# remove stale docker-master.spec
+#rm docker-master.spec
 
-# copy latest rkt.spec to rkt-master.spec
-#cp rkt.spec rkt-master.spec
+# copy latest docker.spec to docker-master.spec
+#cp docker.spec docker-master.spec
 
-# change package name for rkt-master
-#sed -i "s/Name: \%{repo}/Name: \%{repo}-master/" rkt-master.spec
+# change package name for docker-master
+#sed -i "s/Name: \%{repo}/Name: \%{repo}-master/" docker-master.spec
+
+# docker-master Provides: docker
+#sed -i "/Name: %{repo}-master/a Provides: %{repo} = %{version}-%{release}" \
+#    docker-master.spec
 
 # modify build tags for el7
-#sed -i 's/selinux\"/selinux btrfs_noversion\"/' rkt-master.spec
+#sed -i 's/selinux\"/selinux btrfs_noversion\"/' docker-master.spec
 
 # build for el7
-#rpmbuild --define 'dist .el7' -ba rkt-master.spec --nocheck
+#rpmbuild --define 'dist .el7' -ba docker-master.spec --nocheck
 
 # ckoji is a symlink to koji which uses my CentOS koji config
-#~/bin/ckoji build virt7-el7 SRPMS/rkt-master-*.el7.src.rpm
+#~/bin/ckoji build virt7-el7 SRPMS/docker-master-*.el7.src.rpm
+#~/bin/ckoji tag-pkg virt7-docker-master-testing $(ls SRPMS | sed -e "s/\.src.*//")
