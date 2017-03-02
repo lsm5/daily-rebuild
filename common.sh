20cac1d6784f7e4c4c3055e0f75277e9c488fd63
Lokesh Mandvekar <lsm5@redhat.com>
2017-03-02 15:10:23 -0500

    build on fedora and rhel
    - script will detect host distro and rpmbuild for it
    - docker-storage-setup update commented out as per upstream request
    
    Signed-off-by: Lokesh Mandvekar <lsm5@redhat.com>

--- common.sh
+++ common.sh
@@ -5,7 +5,7 @@
 # delete stale packages, tarballs and build dirs
 cleanup_stale ()
 {
-    pushd $PKG_DIR/$DIST/$PACKAGE
+    pushd $PKG_DIR/$PACKAGE
     git clean -dfx
     popd
 }
@@ -13,21 +13,25 @@ cleanup_stale ()
 # update spec changelog and release value
 bump_spec ()
 {
-    export CURRENT_VERSION=$(cat $PACKAGE.spec | grep "Version:" | \
-        sed -e "s/Version: //")
-    if [ "$CURRENT_VERSION" == "$VERSION" ]; then
-        rpmdev-bumpspec -c "$(cat /tmp/$PACKAGE.changelog)" $PACKAGE.spec
+    if [ $PACKAGE == 'docker' ]; then
+        export CURRENT_VERSION=$(cat $PACKAGE.spec | grep "Version:" | \
+            sed -e "s/Version: //")
+        if [ "$CURRENT_VERSION" == "$VERSION" ]; then
+            rpmdev-bumpspec -c "$(cat /tmp/$PACKAGE.changelog)" $PACKAGE.spec
+        else
+            rpmdev-bumpspec -n $VERSION -c "$(cat /tmp/$PACKAGE.changelog)" $PACKAGE.spec
+            sed -i "s/Release: 1\%{?dist}/Release: 1.git\%{shortcommit0}\%{?dist}/" $PACKAGE.spec
+            sed -i "s/$VERSION-1/$VERSION-1.git$SHORTCOMMIT/g" $PACKAGE.spec
+        fi
     else
-        rpmdev-bumpspec -n $VERSION -c "$(cat /tmp/$PACKAGE.changelog)" $PACKAGE.spec
-        sed -i "s/Release: 1\%{?dist}/Release: 1.git\%{shortcommit0}\%{?dist}/" $PACKAGE.spec
-        sed -i "s/$VERSION-1/$VERSION-1.git$SHORTCOMMIT/g" $PACKAGE.spec
+        rpmdev-bumpspec -c "$(cat /tmp/$PACKAGE.changelog)" $PACKAGE.spec
     fi
 }
 
 # rpmbuild
 fetch_and_build ()
 {
-    pushd $PKG_DIR/$DIST/$PACKAGE
+    pushd $PKG_DIR/$PACKAGE
     git checkout $DIST_GIT_TAG
     bump_spec
     spectool -g $PACKAGE.spec
--- env.sh
+++ env.sh
@@ -3,12 +3,14 @@
 export REPO_DIR=~/repositories
 export PKG_DIR=~/repositories/pkgs
 export DIST=$(rpm --eval %{?dist})
-if [ $DIST == 'fedora' ]; then
-    export DIST_PKG="fedpkg"
-    export BUILDDEP="dnf builddep"
-else
+if [ $DIST == '.el7' ]; then
+    export DISTRO="rhel"
     export DIST_PKG="rhpkg"
     export BUILDDEP="yum-builddep"
+else
+    export DISTRO="fedora"
+    export DIST_PKG="fedpkg"
+    export BUILDDEP="dnf builddep"
 fi
 
 while getopts ":p:b:k:" opt; do
