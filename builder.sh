e70087687ef8876beaa0a6625144578b6c7a80f3
Lokesh Mandvekar <lsm5@redhat.com>
2017-03-15 16:21:03 -0400

    pkgs/default.sh builds runc and container-selinux
    Signed-off-by: Lokesh Mandvekar <lsm5@redhat.com>

--- builder.sh
+++ builder.sh
@@ -2,7 +2,18 @@
 
 . env.sh
 
-. pkgs/$PACKAGE.sh
+case "$PACKAGE" in
+    docker)
+        . pkgs/$PACKAGE.sh
+        ;;
+    docker-latest)
+        . pkgs/$PACKAGE.sh
+        ;;
+    *)
+        . pkgs/default.sh
+        ;;
+esac
+
 update_sources_and_spec
 
 . common.sh
