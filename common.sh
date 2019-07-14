a28e189970657ee91e7c4f7864fd07db74f1c087
Lokesh Mandvekar <lsm5@fedoraproject.org>
2019-07-14 22:25:17 +0530

    check repo status
    Signed-off-by: Lokesh Mandvekar <lsm5@fedoraproject.org>

--- common.sh
+++ common.sh
@@ -71,7 +71,10 @@ commit_to_dist_git ()
 push_and_build ()
 {
     pushd $PKG_DIR/$PACKAGE
-    git push -u origin master
+    echo "Pushing to" $DIST_GIT_TAG
+    git push -u origin $DIST_GIT_TAG
+    echo "Checking git status ..."
+    git status
     if [ $? -ne 0 ]; then
         echo "git push FAIL!!!"
         exit 1
