ffc23d3632bafdc0acfc5e84331bdcb0226627ba
Lokesh Mandvekar <lsm5@fedoraproject.org>
2019-07-17 18:42:46 +0530

    use master branch for container-selinux
    Signed-off-by: Lokesh Mandvekar <lsm5@fedoraproject.org>

--- pkgs/default.sh
+++ pkgs/default.sh
@@ -8,9 +8,15 @@ fetch_version_and_commit ()
     pushd $REPO_DIR/$PACKAGE
     git fetch origin
     git checkout origin/master
-    export LATEST_TAG=$(git describe --tags --abbrev=0)
-    export VERSION=$(echo $LATEST_TAG | sed -e 's/v//')
-    export COMMIT=$(git show --pretty=%H -s $(echo $LATEST_TAG))
-    export SHORTCOMMIT=$(c=$COMMIT; echo ${c:0:7})
+    if [ $PACKAGE == container-selinux ]; then
+       export VERSION=$(cat VERSION)
+       export COMMIT=$(git show --pretty=%H -s origin/master)
+       export SHORTCOMMIT=$(c=$COMMIT; echo ${c:0:7})
+    else
+       export LATEST_TAG=$(git describe --tags --abbrev=0)
+       export VERSION=$(echo $LATEST_TAG | sed -e 's/v//')
+       export COMMIT=$(git show --pretty=%H -s $(echo $LATEST_TAG))
+       export SHORTCOMMIT=$(c=$COMMIT; echo ${c:0:7})
+    fi
     popd
 }
