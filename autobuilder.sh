73cf04e038ac49418c39335fd493ae835a63dc11
Lokesh Mandvekar <lsm5@fedoraproject.org>
2019-08-03 17:46:17 +0000

    build bionic and disco versions using a common branch
    Signed-off-by: Lokesh Mandvekar <lsm5@fedoraproject.org>

--- autobuilder.sh
+++ autobuilder.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
-# Get ubuntu version variables
-. /etc/os-release
-
 export NAME="Lokesh Mandvekar (Bot)"
 export EMAIL="lsm5+bot@fedoraproject.org"
 export USER="lsm5+bot"
 export DEBFULLNAME=$NAME
 export DEBEMAIL=$EMAIL
+
+export DISTRO="ubuntu"
+
+if [[ $DISTRO_VERSION == "xenial" ]]; then
+   export DISTRO_VERSION_ID="16.04"
+elif [[ $DISTRO_VERSION == "bionic" ]]; then
+   export DISTRO_VERSION_ID="18.04"
+elif [[ $DISTRO_VERSION == "disco" ]]; then
+   export DISTRO_VERSION_ID="19.04"
+fi
+
 
 cd ~/.ssh
 openssl enc -aes-256-cbc -pbkdf2 -d -in id_rsa.enc -out id_rsa -pass pass:$DECRYPTION_PASSPHRASE
@@ -30,14 +38,14 @@ if [[ $PACKAGE == "cri-o" ]]; then
    export LATEST_SHORTCOMMIT=$(c=$LATEST_COMMIT; echo ${c:0:7})
    export LATEST_VERSION=$(grep 'const Version' version/version.go | sed -e 's/const Version = "//' -e 's/-.*//')
    echo "Checking out branch with debian changes..."
-   git checkout $VERSION_CODENAME-$BRANCH
+   git checkout $DISTRO_VERSION-$BRANCH
    echo "Extracting current commit from deb package..."
    export CURRENT_SHORTCOMMIT=$(dpkg-parsechangelog -c 1 | grep built | sed -e 's/.*built //')
    if [[ $LATEST_SHORTCOMMIT == $CURRENT_SHORTCOMMIT ]]; then
       echo "No new upstream commits. Exiting..."
       exit 0
    else
-      echo "Rebasing $VERSION_CODENAME-$BRANCH on top of commit $LATEST_SHORTCOMMIT for $PACKAGE..."
+      echo "Rebasing $DISTRO_VERSION-$BRANCH on top of commit $LATEST_SHORTCOMMIT for $PACKAGE..."
       git rebase $LATEST_COMMIT
       if [ $? -ne 0 ]; then
          echo "Rebase on commit $LATEST_SHORTCOMMIT failed. Exiting..."
@@ -45,9 +53,9 @@ if [[ $PACKAGE == "cri-o" ]]; then
       fi
       echo "Bumping changelog..."
       if [[ $LATEST_VERSION != $CURRENT_VERSION ]]; then
-         debchange --package "$PACKAGE-$BRANCH" -v "$LATEST_VERSION-1~dev~$ID$VERSION_ID~ppa1" -D $VERSION_CODENAME "bump to $LATEST_VERSION, autobuilt $LATEST_SHORTCOMMIT"
+         debchange --package "$PACKAGE-$BRANCH" -v "$LATEST_VERSION-1~dev~$DISTRO$DISTRO_VERSION_ID~ppa1" -D $DISTRO_VERSION "bump to $LATEST_VERSION, autobuilt $LATEST_SHORTCOMMIT"
       else
-         debchange --package "$PACKAGE-$BRANCH" -i -D $VERSION_CODENAME "autobuilt $LATEST_SHORTCOMMIT"
+         debchange --package "$PACKAGE-$BRANCH" -i -D $DISTRO_VERSION "autobuilt $LATEST_SHORTCOMMIT"
       fi
       git commit -asm "bump to $LATEST_VERSION"
    fi
@@ -56,20 +64,20 @@ else
    export LATEST_TAG=$(git describe --tags --abbrev=0 origin/master)
    export LATEST_VERSION=$(echo $LATEST_TAG | sed -e 's/v//' -e 's/-.*//')
    echo "Checking out branch with debian changes..."
-   git checkout $VERSION_CODENAME 
+   git checkout $DISTRO_VERSION 
    export CURRENT_VERSION=$(dpkg-parsechangelog --show-field Version | sed -e 's/-.*//')
    if [ $LATEST_VERSION == $CURRENT_VERSION ]; then
       echo "No new upstream release. Exiting..."
       exit 0
    else
-      echo "Rebasing $VERSION_CODENAME on top of tag $LATEST_TAG for $PACKAGE..."
+      echo "Rebasing $DISTRO_VERSION on top of tag $LATEST_TAG for $PACKAGE..."
       git rebase $LATEST_TAG
       if [ $? -ne 0 ]; then
          echo "Rebase on tag $LATEST_TAG failed. Exiting..."
          exit 1
       fi
       echo "Bumping changelog..."
-      debchange --package "$PACKAGE" -v "$LATEST_VERSION-1~$ID$VERSION_ID~ppa1" -D $VERSION_CODENAME "bump to $LATEST_VERSION"
+      debchange --package "$PACKAGE" -v "$LATEST_VERSION-1~$DISTRO$DISTRO_VERSION_ID~ppa1" -D $DISTRO_VERSION "bump to $LATEST_VERSION"
       git commit -asm "bump to $LATEST_VERSION"
    fi
 fi
@@ -101,9 +109,9 @@ fi
 
 echo "Pushing changes to gitlab/$PACKAGE..."
 if [[ $PACKAGE == "cri-o" ]]; then
-        GIT_SSH_COMMAND="ssh -i $HOME/.ssh/id_rsa" git push -u gitlab $VERSION_CODENAME-$BRANCH -f
+        GIT_SSH_COMMAND="ssh -i $HOME/.ssh/id_rsa" git push -u gitlab $DISTRO_VERSION-$BRANCH -f
 else
-        GIT_SSH_COMMAND="ssh -i $HOME/.ssh/id_rsa" git push -u gitlab $VERSION_CODENAME -f
+        GIT_SSH_COMMAND="ssh -i $HOME/.ssh/id_rsa" git push -u gitlab $DISTRO_VERSION -f
 fi
 if [ $? -ne 0 ]; then
         echo "Failed to push changes to gitlab. Exiting..."
@@ -114,9 +122,9 @@ echo "Adding github mirror..."
 git remote add github github:lsm5/$PACKAGE.git
 echo "Pushing changes to github/$PACKAGE..."
 if [[ $PACKAGE == "cri-o" ]]; then
-        GIT_SSH_COMMAND="ssh -i $HOME/.ssh/id_rsa" git push -u github $VERSION_CODENAME-$BRANCH -f
+        GIT_SSH_COMMAND="ssh -i $HOME/.ssh/id_rsa" git push -u github $DISTRO_VERSION-$BRANCH -f
 else
-        GIT_SSH_COMMAND="ssh -i $HOME/.ssh/id_rsa" git push -u github $VERSION_CODENAME -f
+        GIT_SSH_COMMAND="ssh -i $HOME/.ssh/id_rsa" git push -u github $DISTRO_VERSION -f
 fi
 if [ $? -ne 0 ]; then
         echo "Failed to push changes to github, not critical. Continuing..."
